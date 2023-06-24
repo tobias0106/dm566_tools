9b6ef88fb15c6da1e94270278e6f842f5d061d13
kris098e <kristian.abrahamsen@cardlay.com>
2023-06-24 18:33:25 +0200

    make it work with empty clusters

--- k-means-lloyd-forgy.py
+++ k-means-lloyd-forgy.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 def manhattan_distance(p1, p2):
-    return sum(abs(a - b) for a, b in zip(p1, p2))
+    return np.sum(np.abs(p1 - p2))
 
 def euclidean_distance(p1, p2):
-    return sum((a - b) ** 2 for a, b in zip(p1, p2)) ** 0.5
+    return np.sqrt(np.sum((p1 - p2) ** 2))
 
 def update_clusters(points, means):
     clusters = [[] for _ in range(len(means))]
@@ -19,8 +19,9 @@ def update_clusters(points, means):
 def update_means(clusters):
     means = []
     for cluster in clusters:
-        mean = np.mean(cluster, axis=0)
-        means.append(mean)
+        if cluster:
+            mean = np.mean(cluster, axis=0)
+            means.append(mean)
 
     return means
 
@@ -35,22 +36,20 @@ def k_means(points, initial_means):
         if np.array_equal(means, new_means):
             clusters = new_clusters
             break
-
         means = new_means
 
     return clusters
 
 # If we have an initial clustering, calculate initial means
+'''
 points = [np.array([(1,5), (2,3)]), np.array([(3,4), (7,7), (10,1)]), np.array([(6,8), (7,8), (7,9)])]
 initial_means = np.array([np.mean(point, axis=0) for point in points])
 points = np.array([[1,5], [2,3], [3,4], [7,7], [10,1], [6,8], [7,8], [7,9]])
-
-
-''' if we have no initial clustering 
-points = np.array([[[2, 0], [3, 0], [4, 0], [10, 0], [11, 0], [12, 0], [20, 0], [25, 0], [30, 0]])
-initial_means = np.array([[2, 0], [4, 0], [6, 0]])
 '''
 
+# If we have no initial clustering
+points = np.array([[2, 0], [3, 0], [4, 0], [10, 0], [11, 0], [12, 0], [20, 0], [25, 0], [30, 0]])
+initial_means = np.array([[2, 0], [4, 0], [6, 0], [10, 0]], dtype=float)
 
 # Run k-means algorithm
 clusters = k_means(points, initial_means)
@@ -58,9 +57,7 @@ clusters = k_means(points, initial_means)
 means = update_means(clusters)
 
 # Print clusters after each iteration
-iteration = 1
-for cluster in clusters:
-    print(f"cluster {iteration}: {cluster}")
-    iteration += 1
+for i, cluster in enumerate(clusters):
+    print(f"cluster {i + 1}: {cluster}")
 
 print("\nfinal means:", means)
